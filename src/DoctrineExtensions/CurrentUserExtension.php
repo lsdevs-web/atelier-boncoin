4781466819acfffd85db788478ee31ddd5bc2471
Laurent <ls.devs.web@gmail.com>
2020-02-07 12:06:41 +0100

    Optimisation de l'extension Doctrine CurrentUserExentension (verif si user et si admin ou non)

--- src/DoctrineExtensions/CurrentUserExtension.php
+++ src/DoctrineExtensions/CurrentUserExtension.php
@@ -8,8 +8,11 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInter
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use App\Entity\Annonce;
+use App\Entity\User;
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Security\Core\Security;
 
 /**
@@ -21,38 +24,50 @@ class CurrentUserExtension implements QueryCollectionExtensionInterface, QueryIt
 
     private $security;
     private $request;
+    private $auth;
 
     /**
      * CurrentUserExtension constructor.
      * @param Security $security
      * @param RequestStack $request
+     * @param AuthorizationCheckerInterface $auth
      */
-    public function __construct(Security $security, RequestStack $request)
+    public function __construct(Security $security, RequestStack $request, AuthorizationCheckerInterface $auth)
     {
         $this->security = $security;
         $this->request = $request;
+        $this->auth = $auth;
     }
 
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
     {
+
+        $this->addWhere($queryBuilder, $resourceClass);
+
+    }
+
+    private function addWhere(QueryBuilder $queryBuilder, string $resourceClass)
+    {
+
         $user = $this->security->getUser();
 
         $data = $this->request->getCurrentRequest()->get("all");
 
 
-        if ($resourceClass === Annonce::class && $data == 0) {
+        if (($resourceClass === Annonce::class && $data == 0) && !$this->auth->isGranted("ROLE_ADMIN") && $user instanceof User) {
             $rootAlias = $queryBuilder->getRootAliases()[0];
             $queryBuilder->andWhere("$rootAlias.user  = :user");
             $queryBuilder->setParameter("user", $user);
 
-//            dd($queryBuilder);
 
         }
+
     }
 
     public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, string $operationName = null, array $context = [])
     {
-        // TODO: Implement applyToItem() method.
+        $this->addWhere($queryBuilder, $resourceClass);
+
     }
 
 }
